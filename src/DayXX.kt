2972366aa0415d080c4e29218c712971c6b3e08e
tteichma <tteichma@users.noreply.github.com>
2023-12-01 13:35:40 +0100

    Added solution for day 01.

---
+++ src/DayXX.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("DayXX_test")
+    check(part1(testInput) == 1) { "Wrong solution: ${part1(testInput)}" }
+    check(part2(testInput) == 1) { "Wrong solution: ${part2(testInput)}" }
+
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
