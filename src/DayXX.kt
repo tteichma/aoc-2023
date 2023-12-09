501a5a61102d168db229dc730a837031068005c7
tteichma <tteichma@users.noreply.github.com>
2023-12-09 10:41:09 +0100

    Update template.

--- src/DayXX.kt
+++ src/DayXX.kt
@@ -1,16 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    data class Helper(val line: String)
+
+
+    fun parseInput(input: List<String>): List<Helper> {
+        return input.map { Helper(it) }
     }
 
-    fun part2(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
+        return parseInput(input).size.toLong()
+    }
+
+    fun part2(input: List<String>): Long {
         return part1(input)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("DayXX_test")
-    check(part1(testInput) == 1) { "Wrong solution: ${part1(testInput)}" }
-    check(part2(testInput) == 1) { "Wrong solution: ${part2(testInput)}" }
+    check(part1(testInput) == 1L) { "Wrong solution: ${part1(testInput)}" }
+    check(part2(testInput) == 1L) { "Wrong solution: ${part2(testInput)}" }
 
     val input = readInput("DayXX")
     part1(input).println()
